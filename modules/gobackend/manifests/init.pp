e68ac4de9d691e8f4fb36d5acbc32046ced4eed1
Andrey Kochnov <andreykochnov@yandex.ru>
2020-10-03 17:36:52 +0300

    root

--- modules/gobackend/manifests/init.pp
+++ modules/gobackend/manifests/init.pp
@@ -14,7 +14,8 @@ class gobackend(
     ports            => ['9000:8099'],
     env              => [
       'POSTS_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/posts?search_path=posts&sslmode=disable', 
-      'UPLOAD_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/upload?search_path=upload&sslmode=disable'],
+      'UPLOAD_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/upload?search_path=upload&sslmode=disable'
+    ],
     restart_service  => true,
     privileged       => true,
     pull_on_start    => true,
