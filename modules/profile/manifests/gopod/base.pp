1adf1119f636f1e2f14fa11b5dab816483e90c65
Andrey Kochnov <andreykochnov@yandex.ru>
2020-10-21 22:14:27 +0300

    Switch on posts services

--- modules/profile/manifests/gopod/base.pp
+++ modules/profile/manifests/gopod/base.pp
@@ -3,52 +3,70 @@ class profile::gopod::base {
     version => 'latest',
   }
 
+  # Service posts (main)
   $posts_envs =  [
-      'POSTS_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/posts?search_path=posts&sslmode=disable',
-      'UPLOAD_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/upload?search_path=upload&sslmode=disable',
-      'FILE_PATH=/storage/files',
-      'PHOTO_PATH=/storage/photos',
+    'POSTS_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/posts?search_path=posts&sslmode=disable',
+    'UPLOAD_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/upload?search_path=upload&sslmode=disable',
+    'FILE_PATH=/storage/files',
+    'PHOTO_PATH=/storage/photos',
   ]
-
-  # gobackend::service { 'posts_master':
-  #   port    => '9100',
-  #   service => 'posts',
-  #   branch  => 'master',
-  #   env     => $posts_envs
-  # }
-
+  gobackend::service { 'posts_master':
+    port    => '9100',
+    service => 'posts',
+    branch  => 'master',
+    env     => $posts_envs
+  }
   gobackend::service { 'posts_dev':
     port    => '9101',
     service => 'posts',
     branch  => 'dev',
     env     => $posts_envs
   }
+  gobackend::service { 'posts_predev':
+    port    => '9102',
+    service => 'posts',
+    branch  => 'predev',
+    env     => $posts_envs
+  }
+  # ----------------------
 
-  # gobackend::service { 'posts_predev':
-  #   port    => '9102',
-  #   service => 'posts',
-  #   branch  => 'predev',
-  #   env     => $posts_envs
-  # }
-
+  # Service group
+  $group_env = [
+      'GROUP_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/groups?search_path=groups&sslmode=disable'
+  ]
+  gobackend::service { 'group_main':
+    port    => '9200',
+    service => 'group',
+    branch  => 'main',
+    env     => $group_env,
+  }
   gobackend::service { 'group_dev':
     port    => '9201',
     service => 'group',
     branch  => 'dev',
-    env     => [
-      'GROUP_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/groups?search_path=groups&sslmode=disable'
-    ],
+    env     => $group_env,
   }
+  # ----------------------
 
+  # Service interview
+  $interview_env = [
+      'INTERVIEW_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/posts?search_path=posts&sslmode=disable'
+  ]
+  gobackend::service { 'interview_main':
+    port    => '9300',
+    service => 'interview',
+    branch  => 'main',
+    env     => $interview_env,
+  }
   gobackend::service { 'interview_dev':
     port    => '9301',
     service => 'interview',
     branch  => 'dev',
-    env     => [
-      'INTERVIEW_DB_CONNECTION_STRING=postgres://postgres:postgres@185.255.134.117:5432/posts?search_path=posts&sslmode=disable'
-    ],
+    env     => $interview_env,
   }
+  # ----------------------
 
+  # Cron jobs
   cron { 'docker_clear':
     command => 'sudo /bin/docker container prune -f && sudo /bin/docker image prune -f',
     user    => 'deploy',
