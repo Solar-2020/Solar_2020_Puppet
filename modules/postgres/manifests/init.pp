2e95c638c537fbab64b9d4b036dcc39e06a9c057
Andrey Kochnov <andreykochnov@yandex.ru>
2020-10-27 01:44:48 +0300

    cron for sessions

--- modules/postgres/manifests/init.pp
+++ modules/postgres/manifests/init.pp
@@ -16,4 +16,22 @@ class postgres {
     require   => Package['postgresql13-server'],
     subscribe => File['/var/lib/pgsql/13/data/pg_hba.conf']
   }
+
+  file { '/var/log/solar_pg.sessions_cleanup.log':
+    ensure => present,
+    owner  => 'postgres',
+    group  => 'postgres',
+    mode   => '0644'
+  }
+
+  cron { 'postgres_sessions_cleanup':
+    command => "sudo psql ${facts['db_root']}/auth -c 
+      'with del as (DELETE FROM auth.sessions where expiration < NOW() returning *) 
+        select count(cookie) from del;' 
+      > /var/log/solar_pg.sessions_cleanup.log",
+    user    => 'deploy',
+    hour    => '*',
+    minute  => '*',
+    require => File['/var/log/solar_pg.sessions_cleanup.log']
+  }
 }
