faae72adae436c134988324c8957507a4a663a56
Andrey Kochnov <andreykochnov@yandex.ru>
2020-11-12 23:33:17 +0300

    try

--- modules/profile/manifests/gopod/base.pp
+++ modules/profile/manifests/gopod/base.pp
@@ -99,14 +99,13 @@ class profile::gopod::base {
   # Service group
   $group_env = [
       "GROUP_DB_CONNECTION_STRING=${db_root}/groups?search_path=groups&sslmode=disable",
+      "INVITE_GROUP_PREFIX_ADDRESS=https://${hostname}/welcome"
   ]
   gobackend::service { 'group_dev':
     port      => $go_dev_env['sub_ports']['group'],
     service   => 'group',
     branch    => 'dev',
-    env       => concat($commod_env_dev, [
-      "GROUP_DB_CONNECTION_STRING=${db_root}/groups?search_path=groups&sslmode=disable",
-    ]),
+    env       => concat($commod_env_dev, $group_env),
     image_tag => $image_tag,
   }
   # ----------------------
@@ -181,9 +180,7 @@ class profile::gopod::base {
     port      => $go_master_env['sub_ports']['group'],
     service   => 'group',
     branch    => 'main',
-    env       => concat($commod_env_prod, [
-      "GROUP_DB_CONNECTION_STRING=${db_root}/groups?search_path=groups&sslmode=disable",
-    ]),
+    env       => concat($commod_env_prod, $group_env),
     image_tag => $image_tag,
   }
   # ----------------------
